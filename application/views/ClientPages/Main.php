c5beb943c115f5d343168d54f318640fbb9798a8
Christiann12 <christianpaulpili@yahoo.com>
2021-12-09 21:08:53 +0800

    feat: Create footer
    Created footer and improved header UI responsive design

--- application/views/ClientPages/Main.php
+++ application/views/ClientPages/Main.php
@@ -4,4 +4,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <div>
     <P>Home Page</P>
+    <!-- <P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P><P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P><P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P>
+    <P>Home Page</P> -->
+
 </div>
--- application/views/HeaderNFooter/Header.php
+++ application/views/HeaderNFooter/Header.php
@@ -11,72 +11,75 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <!-- Developer Created Css -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>application/assets/css/header.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>application/assets/css/footer.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>application/assets/css/Homepage.css">
 	</head>
 <body style="font-family: Montserrat;" >
 <!-- oncontextmenu="return false;" -->
-  
 
-	<!-- Top Bar -->
-  <div class="d-flex flex-lg-row flex-column justify-content-lg-around top-bar">
-    <div><p class="top-bar-title" style="">ODMS Enterprise</p></div>
-    <div class="">
-      <i class="fa fa-phone icon"aria-hidden="true">
-        <div class="text-container">
-          <p class="text-title">Call Us</p>
-          <p class="pb-lg-0 pb-3 text-desc">09xx-xxx-xxxx</p>
-        </div>
-      </i>  
+  <div class="row top-bar">
+    <div class="col-lg-3 col-md-12  d-flex justify-content-center"><p class="top-bar-title" style="">ODMS Enterprise</p></div>
+    <div class="col-lg-3 col-md-4 col-6 d-flex justify-content-center">
+      <div class="divicon">
+        <i class="fa fa-phone icon"aria-hidden="true">
+          <div class="text-container">
+            <p class="text-title">Call Us</p>
+            <p class="pb-lg-0 pb-3 text-desc">09xx-xxx-xxxx</p>
+          </div>
+        </i>  
+      </div>
     </div>
-    <div class="">
-      <i class="fa fa-envelope icon" aria-hidden="true">
-        <div class="text-container">
-          <p class="text-title">Email Us</p>
-          <p class="pb-lg-0 pb-3 text-desc">company@domain.com</p>
-        </div>
-      </i>
+    <div class="col-lg-3 col-md-4 col-6 d-flex justify-content-center">
+      <div class="divicon">
+        <i class="fa fa-envelope icon" aria-hidden="true">
+          <div class="text-container">
+            <p class="text-title">Email Us</p>
+            <p class="pb-lg-0 pb-3 text-desc">company@domain.com</p>
+          </div>
+        </i>
+      </div>
     </div>
-    <div class="">
-      <i class="fa fa-clock-o icon" aria-hidden="true">
-        <div class="text-container">
-            <p class="text-title">Opening Hours</p>
-            <p class="pb-lg-0 pb-3 text-desc">5:00am - 8:00pm</p>
-        </div>
-      </i>
+    <div class="col-lg-3 col-md-4 col-12 d-flex justify-content-lg-center justify-content-md-center justify-content-sm-center justify-content-center">
+      <div class="divicon">
+        <i class="fa fa-clock-o icon" aria-hidden="true">
+          <div class="text-container">
+              <p class="text-title">Opening Hours</p>
+              <p class="pb-lg-0 pb-3 text-desc">5:00am - 8:00pm</p>
+          </div>
+        </i>
+      </div>
     </div>
   </div>
 	<!-- End Top Bar -->
 	<!-- Navigation -->
 
-	<nav class="navbar navbar-expand-lg navbar-dark navcolor"> 
-    <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse mr-md-5 mr-sm-3 " id="navbarText">
-      <ul class="navbar-nav m-lg-auto ml-md-auto ml-sm-auto ml-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About Us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact Us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Shop</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Ping</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Financial Assistance</a>
-        </li>
-      </ul>
-      
-    </div>
-    
-  </nav>
+
+  <nav class="navbar navbar-expand-lg navbar-dark navcolor">
+        <div class="container">
+            <button class="navbar-toggler ml-auto" type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+  
+            <div class="collapse navbar-collapse"></div>
+  
+            <div class="collapse navbar-collapse" 
+                 id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item active"><a class="nav-link" href="#">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">About Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Contact Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Shop</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Ping</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Financial Assistance</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 	
 	<!-- End of Navigation -->
 
