5a680e57bf367685414bac148c0b5fdfbe66ac64
Christiann12 <christianpaulpili@yahoo.com>
2021-12-16 17:46:07 +0800

    feat: created ping page
    created ping page base on the interface design

---
+++ application/views/ClientPages/ping.php
@@ -0,0 +1,84 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<div class="pingBanner container mx-auto">
+    <div class="row">
+        <div class="col-12 col-md-6 content ">
+            <p class="heading1">WHAT IS PING?</p>
+            <p class="heading2 pt-md-4 pt-0">We’ve Got Your Back!</p>
+            <p class="heading3 pt-md-4 pt-0">This section helps the users of the website quickly alert ODMS Enterprise in time of emergencies. A short description of what this section does will go here.</p>
+            <button type="button" class="btn btn-warning mt-md-4 mt-0" style="">Learn More!</button>
+        </div>
+        <div class="col-12 col-md-6 img" style="background-image: url('<?php echo base_url(); ?>application/assets/images/ClientPagesImages/ping.gif'); ">
+
+        </div>
+    </div>
+</div>
+<div class="pingSteps" style="background-image: url('<?php echo base_url(); ?>application/assets/images/ClientPagesImages/electricians1.jpg'); ">
+    
+    <div class="content">
+        
+        <p class="heading1">HOW DOES IT WORK?</p>
+
+        <div class="row container mx-auto">
+
+            <div class="col-lg-3 col-md-6 col-12 ">
+               <div class="col-12 innerCol">
+                    <div class="number mx-auto">
+                        <p>1</p>
+                    </div>
+                    <p class="text1">Click the Ping button below the page. An input dialog box will pop-up.</p>
+               </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 ">
+                    
+               <div class="col-12 innerCol">
+                    <div class="number mx-auto">
+                        <p>2</p>
+                    </div>
+                   <p class="text1">Fill-out the required details in the form.</p>
+               </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 ">
+                    
+                <div class="col-12 innerCol">
+                <div class="number mx-auto">
+                        <p>3</p>
+                    </div>
+                    <p class="text1">Make sure that all the required details are filled out and correct. Once done, click the Submit button.</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 ">
+                    
+                <div class="col-12 innerCol">
+                    <div class="number mx-auto">
+                        <p>4</p>
+                    </div>
+                    <p class="text1">Your request will now reflect in the admin side. An email will be sent to you once your request is acknowledged.</p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+<!-- <div class="pingNow container mx-auto" style="">
+    <div class="row" style="height: 100%;">
+        
+        <div class="col-12 col-md-6 content">
+            <p class="heading1">Have An Emergency?</p>
+            <button type="button" class="btn btn-warning mt-md-4 mt-0" style="">PING ODMS</button>
+        </div>
+    </div>
+</div> -->
+<div class="pingNow container mx-auto">
+    <div class="row">
+        <div class="col-12 col-md-6 img" style="background-image: url('<?php echo base_url(); ?>application/assets/images/ClientPagesImages/ping 2.gif'); "></div>
+        <div class="col-12 col-md-6 content ">
+            <p class="heading1">Have An Emergency?</p>
+            <button type="button" class="btn btn-warning mt-md-4 mt-0" style="">Learn More!</button>
+        </div>        
+    </div>
+</div>
