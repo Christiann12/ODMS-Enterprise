a3314017b3db57ef52c2321e6c44d9485844488f
Aian Mark Antonio <aianmarkantonio@gmail.com>
2022-01-29 21:53:36 +0800

    feat:Created Services Inventory(Admin)
    Added Create, Update and Delete Function to Services Inventory - Admin Side

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -23,6 +23,7 @@ class Admin extends CI_Controller {
 		$this->load->model('ping_model');
 		$this->load->model('support_model');
 		$this->load->model('fACompanies_model');
+		$this->load->model('srvcsinventory_model');
 		$this->load->helper('url');
 		$this->load->library('session');
 		 
@@ -457,21 +458,6 @@ class Admin extends CI_Controller {
 		echo json_encode($output);
 	}
 
-	public function servicesInventory()
-	{
-		$data['param'] ='servicesInventory';
-		$this->load->helper('url');
-		
-		if($this->session->has_userdata('adminId')){
-			$this->load->view('HeaderNFooter/HeaderAdmin.php');
-			$this->load->view('AdminPages/wrapper.php', $data);
-			$this->load->view('HeaderNFooter/FooterAdmin.php');
-		}
-		else{
-			redirect('login');
-		}
-	}
-
 	public function financialAssistance()
 	{
 		$data['param'] ='financialAssistance';
@@ -864,6 +850,193 @@ class Admin extends CI_Controller {
 		);
 		echo json_encode($output);
 	}
+
+	// Ginawa ko
+	// show and save create data for services inventory page
+	public function servicesInventory()
+	{
+		// Screen to open
+		$data['param'] ='servicesInventory';
+
+		//Form Validation
+		$this->form_validation->set_rules('srvcsTitle', 'Title' ,'required|max_length[30]');
+		$this->form_validation->set_rules('srvcsDesc', 'Description' ,'required|max_length[255]');
+		if (empty($_FILES['attachment']['name'])){
+			$this->form_validation->set_rules('attachment', 'Attachment' ,'required');
+		}
+		$this->form_validation->set_rules('srvcsPrice', 'Price' ,'required|max_length[30]');
+		$this->form_validation->set_rules('srvcsAvailability', 'Service Availability' ,'required');
+		
+		//load config for upload library
+		$config['upload_path']   = APPPATH.'assets/attachments/';
+		$config['allowed_types'] = 'jpg|jpeg|jpe|png';
+		$config['max_size']      = 0;
+		$config['max_width']     = 0;
+		$config['max_height']    = 0;
+		$config['overwrite']     = false;
+		
+		// Helpers
+		$this->load->helper('url');
+		$this->load->library('upload', $config);
+		$filename = "";
+		
+		$name = 'attachment';
+		// StoreData
+		$data['document'] = (object)$postData = array( 
+			'srvcsId' => "SRVC-".$this->randStrGen(2,7),
+            'srvcsTitle' => $this->input->post('srvcsTitle'),
+            'srvcsDesc' => $this->input->post('srvcsDesc'),
+            'srvcsPrice' => $this->input->post('srvcsPrice'),
+			'srvcsAvailability' => $this->input->post('srvcsAvailability')
+        ); 
+		// SendToDatabase
+		if($this->form_validation->run() === true){
+			if ( ! $this->upload->do_upload($name) ) {
+                $this->session->set_flashdata('error',$this->upload->display_errors());
+            } 
+			else {
+                $upload =  $this->upload->data();
+                
+				$postData['srvcsPicture'] = $upload['file_name'];
+				if($this->srvcsinventory_model->create($postData)){
+					$this->session->set_flashdata('success','Add Successful');
+				}
+				else {
+					$this->session->set_flashdata('error','Add Failed');
+				}
+            }
+			redirect('admin/servicesInventory');
+		}
+		// OpenPage
+		else{
+			if($this->session->has_userdata('adminId')){
+				$this->load->view('HeaderNFooter/HeaderAdmin.php');
+				$this->load->view('AdminPages/wrapper.php', $data);
+				$this->load->view('HeaderNFooter/FooterAdmin.php');
+			}
+			else{
+				redirect('login');
+			}
+		}
+	}
+	// update services inventory record
+	public function updateServicesInventoryRecord()
+	{
+		// screen to open
+		$data['param'] ='servicesInventory';
+		$this->form_validation->set_rules('srvcsTitle', 'Title' ,'required|max_length[30]');
+		$this->form_validation->set_rules('srvcsDesc', 'Description' ,'required|max_length[255]');
+		$this->form_validation->set_rules('srvcsPrice', 'Price' ,'required|max_length[30]');
+		$this->form_validation->set_rules('srvcsAvailability', 'Service Availability' ,'required');
+		//load config for upload library
+		$config['upload_path']   = APPPATH.'assets/attachments/';
+		$config['allowed_types'] = 'jpg|jpeg|jpe|png';
+		$config['max_size']      = 0;
+		$config['max_width']     = 0;
+		$config['max_height']    = 0;
+		$config['overwrite']     = false;
+		
+
+		// Helpers
+		$this->load->helper('url');
+		$this->load->library('upload', $config);
+		// StoreData
+		$data['document'] = (object)$postData = array( 
+			'srvcsId' => $this->input->post('srvcsId'),
+            'srvcsTitle' => $this->input->post('srvcsTitle'),
+            'srvcsDesc' => $this->input->post('srvcsDesc'),
+            'srvcsPrice' => $this->input->post('srvcsPrice'),
+			'srvcsAvailability' => $this->input->post('srvcsAvailability')
+        ); 
+		$name = 'attachment';
+		// SendToDatabase
+		if($this->form_validation->run() === true){
+			//check if there's an update with the image
+			if (!empty($_FILES['attachment']['name'])){
+				//if upload fails abort process and display error
+				if ( ! $this->upload->do_upload($name) ) {
+					$this->session->set_flashdata('error',$this->upload->display_errors());
+				} 
+				//if upload success update the database with the correct filename
+				else {
+					$upload =  $this->upload->data();
+					
+					$postData['srvcsPicture'] = $upload['file_name'];
+					if($this->srvcsinventory_model->updateSrvcsItm($postData)){
+						$this->session->set_flashdata('success','Edit Successful');
+						unlink(APPPATH.'assets/attachments/'.$this->input->post('fileName'));
+					}
+					else{
+						$this->session->set_flashdata('error','Edit Failed');
+					}
+				}
+				
+			}
+			//if not continue with the normal update process
+			else{
+				if($this->srvcsinventory_model->updateSrvcsItm($postData)){
+					$this->session->set_flashdata('success','Edit Successful');
+				}
+				else{
+					$this->session->set_flashdata('error','Edit Failed');
+				}
+			}
+			redirect('admin/servicesInventory');
+		}
+		else{
+			$this->load->view('HeaderNFooter/HeaderAdmin.php');
+			$this->load->view('AdminPages/wrapper.php', $data);
+			$this->load->view('HeaderNFooter/FooterAdmin.php');
+		}
+	}
+	// delete services record
+	public function deleteSrvcsInventoryRecord(){
+		//get file name to remove picture as the record is deleted
+		$query = $this->srvcsinventory_model->getInvDataById($this->uri->segment(3));
+		$file = $query->srvcsPicture;
+		if($this->srvcsinventory_model->deleteSrvcsItm($this->uri->segment(3))){
+			unlink(APPPATH.'assets/attachments/'.$file);
+			$this->session->set_flashdata('success','Delete Success');
+		}
+		else{
+			$this->session->set_flashdata('error','Delete Failed');
+		}
+		redirect('admin/servicesInventory');
+	}
+	// fetch data for inventory data table
+	public function servicesInventoryAjax(){
+		//helpers
+		$this->load->helper('url');
+		//load query
+		$list = $this->srvcsinventory_model->getInvTable($this->input->post('txtSearch'));
+		//variable initializations
+		$data = array();
+		$no = $_POST['start'];
+		//iterate per record and organize by row
+		foreach($list as $services){
+			$no++;
+			$row = array();
+			$row[] = $no;
+			$row[] = $services->srvcsId;
+			$row[] = $services->srvcsTitle;
+			$row[] = $services->srvcsDesc;
+			$row[] = "PHP ".$services->srvcsPrice;
+			$row[] = $services->srvcsAvailability;
+			//responsible for the additions of action button in the last row
+			$row[] = '<a href="#" data-toggle="modal" data-target="#modal2" data-file="'.$services->srvcsPicture.'" data-id="'.$services->srvcsId.'" data-name="'.$services->srvcsTitle.'" data-desc="'.$services->srvcsDesc.'" data-price="'.$services->srvcsPrice.'" data-cat="'.$services->srvcsAvailability.'" class="btn btn-xs btn-success"><i class="fa fa-edit"  data-placement="top" title="View"></i></a>
+					<a href="'.base_url('admin/deleteSrvcsInventoryRecord/'.$services->srvcsId.'').'" class="btn btn-xs btn-danger"><i class="fa fa-trash" data-toggle="tooltip" data-placement="top" title="View"></i></a>';
+			$data[] = $row;
+		}
+		//carries the values to the view
+		$output = array(
+			"draw" => $_POST['draw'],
+			"recordsTotal" => $this->srvcsinventory_model->count($this->input->post('txtSearch')),
+			"recordsFiltered" => $this->srvcsinventory_model->count_filtered($this->input->post('txtSearch')),
+			"data" => $data
+		);
+		echo json_encode($output);
+	}
+	
 	// generate random string for id
 	public function randStrGen($mode = null, $len = null){
         $result = "";
---
+++ application/models/srvcsinventory_model.php
@@ -0,0 +1,124 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class srvcsinventory_model extends CI_Model {
+    //global variable for table name
+    private $table = "services";
+    //column order basis
+    var $column_order = array(null,'srvcsId','srvcsTitle','srvcsDesc','srvcsPrice','srvcsAvailability'); //set column field database for datatable orderable
+    //default column order
+    var $order = array('services.srvcsId' => 'asc');
+    public function __construct() {
+        parent::__construct();
+
+        //create table if it doesn't exist
+        if (!$this->db->table_exists($this->table) )
+        {
+            $this->load->dbforge();
+            // define table fields
+            $fields = array(
+                'srvcsId' => array(
+                'type' => 'VARCHAR',
+                'constraint' =>20,
+                ),
+                'srvcsTitle' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 50
+                ),
+                'srvcsDesc' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'default' => 'No desc'
+                ),
+                'srvcsPicture' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 255,
+                    'default' => 'Unverified'
+                ),
+                'srvcsPrice' => array(
+                'type' => 'Decimal',
+                'constraint' => '50,2',
+                'default' => 0
+                ),
+                'srvcsAvailability' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 255,
+                    'default' => 'Unverified'
+                )
+
+            ); 
+            $this->dbforge->add_field($fields);
+            // define primary key
+            $this->dbforge->add_key('srvcsId', TRUE);
+            // create table
+            $this->dbforge->create_table($this->table);
+        }
+    }
+    //create single record
+    public function create($data = []){	 
+        return $this->db->insert($this->table,$data);
+	}
+    //load data for the table
+    public function servicesInventoryTable($searchKey=''){
+        
+        $this->db->select('*');
+        $this->db->from($this->table);
+        if ($searchKey!=''){
+			$this->db->group_start();
+			$this->db->like("services.srvcsId", $searchKey);
+            $this->db->or_like("services.srvcsTitle", $searchKey);
+			$this->db->or_like("services.srvcsDesc", $searchKey);
+            $this->db->or_like("services.srvcsPrice", $searchKey);
+			$this->db->group_end();
+		}
+        if(isset($_POST['order'])){
+			$this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+		} 
+		else if(isset($this->order)){
+			$order = $this->order;
+			$this->db->order_by(key($order), $order[key($order)]);
+		}
+       
+    }
+    //get data for the table
+    public function getInvTable($searchKey=''){
+        $this->servicesInventoryTable($searchKey);
+		if($_POST['length'] != -1)
+		$this->db->limit($_POST['length'], $_POST['start']);
+		$query = $this->db->get();
+		return $query->result();
+    }
+    //count queries for table record infromation
+    public function count($searchKey='')
+	{
+		$this->servicesInventoryTable($searchKey);
+		return $this->db->count_all_results();
+	}
+    //count filtered queries for table record information
+    public function count_filtered($searchKey=''){
+		$this->servicesInventoryTable($searchKey);
+		$query = $this->db->get();
+		return $query->num_rows();
+	}
+    //update query for services
+    public function updateSrvcsItm($data = []){
+        return $this->db->where('srvcsId',$data['srvcsId'])->update($this->table,$data); 
+    }
+    //delete per id services
+    public function deleteSrvcsItm($id = null){
+        $this->db->where('srvcsId',$id)->delete($this->table);
+		if ($this->db->affected_rows()) {
+			return true;
+		} 
+        else {
+			return false;
+		}
+    }
+    //get all data in the table and display in the view
+    public function getInvData(){
+        return $this->db->select("*")->from($this->table)->get()->result();
+    } 
+    //search table per id
+    public function getInvDataById($id = null){
+        return $this->db->select("*")->from($this->table)->where('srvcsId',$id)->get()->row();
+    }    
+}
