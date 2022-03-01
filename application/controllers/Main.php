b489b32c2a429f98af17ef71aa44014de35de2a8
Christiann12 <christianpaulpili@yahoo.com>
2022-03-01 19:55:15 +0800

    Update
    Update

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -9,7 +9,7 @@ class Main extends CI_Controller {
 		date_default_timezone_set('Asia/Singapore');
 		$this->load->model('inventory_model');
 		$this->load->model('Cart_model');
-		$this->load->model('ping_model');
+		$this->load->model('Ping_model');
 		$this->load->model('Support_model');
 		$this->load->model('FACompanies_model');
 		$this->load->model('Srvcsinventory_model');
